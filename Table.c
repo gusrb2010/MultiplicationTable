80d31b5713ead4f15d77020c567f7e6065d6236b
Luke <luke@MacBook-Air.local>
2020-02-26 18:33:33 -0600

    infinite loop for repeated calculations

--- Table.c
+++ Table.c
@@ -8,52 +8,67 @@
 // 3  | 0  3  6  9  12 15
 // 4  | 0  4  8  12 16 20
 
-int main() {
+int main()
+{
    int number; // stores user input
-   // display welcome message
-   printf("Welcome to Multiplication Table Generator!\n");
-   // prompt user for input
-   printf("Please enter integer value: ");
-   // read and store input
-    scanf("%d", &number);
-    // displays output, indicates which table will be made
-    printf("Calculating Multiplication Table for... %d\n", number);
-    printf("     "); //  top row, left hand spacing, 5 spaces
+   int welcome = 0; // use to display welcome message once
+   // infinite loop for easy recalculations
+   while (1)
+   {
+      // display welcome message
+      printf("Welcome to Multiplication Table Generator!\n");
+      // terminate program prompt
+      printf("Press CTRL-C to quit.\n");
+      // prompt user for input
+      printf("Please enter integer value: ");
+      // read and store input
+      scanf("%d", &number);
+      // displays output, indicates which table will be made
+      printf("Calculating Multiplication Table for... %d\n", number);
+      printf("     "); //  top row, left hand spacing, 5 spaces
 
-    // loop through top row integers
-   for(int i = 0; i <= number; i++){
-      printf("%d  ", i);
-   }
-   printf("\n"); // increment to border row (+-----)
-   printf("   +"); //  border row, left hand spacing, 3 spaces
-   for(int i = 0; i <= number; i++){
-      printf("---");
-   }
-   printf("\n"); // increment to 0 row
-   // iterate through remaining rows
-   for(int i = 0; i <= number; i++){
-      // iterate through columns of each row
-      for(int j = 0; j <= number; j++){
-         int result = i*j;
-         // handle first column
-         if(j == 0){
-            printf("%d", i);
-            printf("  | ");
-            printf("%d  ",result);
-         }
-         // handle all remaining columns
-         else{ 
-            // manage spacing for 2-digit ints
-            if(result < 10){
+      // loop through top row integers
+      for (int i = 0; i <= number; i++)
+      {
+         printf("%d  ", i);
+      }
+      printf("\n");   // increment to border row (+-----)
+      printf("   +"); //  border row, left hand spacing, 3 spaces
+      for (int i = 0; i <= number; i++)
+      {
+         printf("---");
+      }
+      printf("\n"); // increment to 0 row
+      // iterate through remaining rows
+      for (int i = 0; i <= number; i++)
+      {
+         // iterate through columns of each row
+         for (int j = 0; j <= number; j++)
+         {
+            int result = i * j;
+            // handle first column
+            if (j == 0)
+            {
+               printf("%d", i);
+               printf("  | ");
                printf("%d  ", result);
             }
-            else{
-               printf("%d ", result);
+            // handle all remaining columns
+            else
+            {
+               // manage spacing for 2-digit ints
+               if (result < 10)
+               {
+                  printf("%d  ", result);
+               }
+               else
+               {
+                  printf("%d ", result);
+               }
             }
          }
+         printf("\n"); // increment to next row
       }
-      printf("\n"); // increment to next row
    }
-
    return 0;
 }
